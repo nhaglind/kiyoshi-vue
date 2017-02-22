aa31d07811c917ec13fe2d00d6c024bbb5284054
Nick Haglind <nhaglind@gmail.com>
2017-02-22 08:45:50 -0600

    using vue instead of react

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <extra></extra>
+    <test></test>
+  </div>
+</template>
+
+<script>
+import test from './components/test'
+import extra from './components/extra'
+export default {
+  name: 'app',
+  components: {
+    test,
+    extra
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/extra.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="extra">
+    <img src="../assets/risa.png">
+  </div>
+</template>
